7dd1a28d7b1857a39e84c7d6af493c39e0ea451f
Mike Manzo <mike.manzo@quantumjoker.com>
2026-01-11 12:28:49 -0500

    Updated copyight info

--- BF6StatsTracker/Views/IntelligenceView.swift
+++ BF6StatsTracker/Views/IntelligenceView.swift
@@ -1,4 +1,14 @@
 //
+// This file is part of BF6StatsTracker.
+//
+// BF6StatsTracker is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3 or later.
+//
+// Copyright (c) 2026 CitizenCoder
+//
+
+//
 //  IntelligenceView.swift
 //  BF6StatsTracker
 //
